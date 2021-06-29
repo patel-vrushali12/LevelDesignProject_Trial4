8c59b8811a98d925e5ffa6a02476570266629bf6
Vrushali <vrushalimsu@gmail.com>
2021-06-29 10:57:20 -0400

    New Scripts for Picker for Finish and backtracking)
    The picker now doesnt reappear if backtracked and reseerts on finish

---
+++ Assets/Scripts/ResetSave.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ResetSave : MonoBehaviour
+{
+    private void Awake()
+    {
+        UniqueItemPicker.Reset();
+    }
+}
---
+++ Assets/Scripts/UniqueItemPicker.cs
@@ -0,0 +1,51 @@
+using MoreMountains.CorgiEngine;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PickerInfo
+{
+    public string sceneName;
+    public Vector3 position;
+
+    public PickerInfo(string sceneName, Vector3 position)
+    {
+        this.sceneName = sceneName;
+        this.position = position;
+    }
+}
+
+public class UniqueItemPicker : InventoryPickableItem
+{
+    static List<PickerInfo> usedPickers = new List<PickerInfo>();
+
+    private void Awake()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        foreach (var pickerInfo in usedPickers)
+        {
+            if (pickerInfo.sceneName != currentSceneName)
+            {
+                continue;
+            }
+
+            if (Vector3.Distance(pickerInfo.position, transform.position) < 0.5f)
+            {
+                Destroy(gameObject);
+                break;
+            }
+        }
+    }
+
+    protected override void PickSuccess()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        usedPickers.Add(new PickerInfo(currentSceneName, transform.position));
+        base.PickSuccess();
+    }
+
+    public static void Reset()
+    {
+        usedPickers.Clear();
+    }
+}
