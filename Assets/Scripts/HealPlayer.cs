d66d64b040a6e7ec4ed2a160025ff0cd7634cd5a
Vrushali <vrushalimsu@gmail.com>
2021-06-30 19:28:51 -0400

    Adding HP to Collectible Treasure with other minor changes

---
+++ Assets/Scripts/HealPlayer.cs
@@ -0,0 +1,33 @@
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+public class HealPlayer : MonoBehaviour
+{
+    [SerializeField] int healValue = 10;
+
+    private void Awake()
+    {
+        var collider = GetComponent<Collider2D>();
+        if (collider == null)
+        {
+            Debug.LogError("Missing a 2D collider.");
+        }
+        else
+        {
+            collider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var health = collision.gameObject.GetComponent<Health>();
+            if (health != null && health.CurrentHealth < health.MaximumHealth)
+            {
+                health.GetHealth(healValue, gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
